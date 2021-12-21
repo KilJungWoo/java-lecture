7c4737a3c3f02c804235c57397e7c0d0bdf2da9d
스폰지밥 <sado816@naver.com>
2021-12-21 21:21:36 +0900

    리터럴 상수

--- Day03/src/ConstExample.java
+++ Day03/src/ConstExample.java
@@ -8,6 +8,18 @@ public class ConstExample {
 		
 		age = 20;
 		System.out.println(age);
+		
+		final double PI = 3.14;
+		System.out.println(PI);
+		
+		PI = 3.20; // 상수는 변경할수 없으므로 에러
+		System.out.println(PI);
+		
+		
+		
+		
+		
+		
 
 	}
 
---
+++ Day03/src/LiteralExample.java
@@ -0,0 +1,11 @@
+
+public class LiteralExample {
+	
+   public static void main(String[] args) {
+	
+	   String desc = "\"길정우\" \n 집에 가서\n 잠이나 \t자";
+	   System.out.println(desc);
+	  
+}
+
+}
