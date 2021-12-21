97bc1252ae6acb3271005bc62cd2a76149e3b988
스폰지밥 <sado816@naver.com>
2021-12-21 20:52:30 +0900

    문자열 객체

--- Day03/src/GitExample.java
+++ Day03/src/GitExample.java
@@ -2,8 +2,14 @@
 public class GitExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
 
 	}
 
 }
+// 끌고오기(서버에서)
+// Clone(맨 처음 한번) -> Fetch -> Pull
+
+// 밀어넣기 (서버로)
+// 소스수정/추가 -> stage -> commit -> push
---
+++ Day03/src/StringExample.java
@@ -0,0 +1,20 @@
+
+public class StringExample {
+	
+	public static void main(String[] args ) {
+		
+		// 변수 선언, 사용
+		// 데이터 타입(자료형) 변수명; <-- 선언
+		// 데이터 타입(자료형) 변수명 = 값; <-- 선언과 동시에 초기화
+		
+		String name;
+		name = "길정우";
+		
+		//문자형은 "정수형"
+		char a = 'A';
+		
+		System.out.println(name + "는 집이 흑석동이다." + 1);
+		
+	}
+
+}
