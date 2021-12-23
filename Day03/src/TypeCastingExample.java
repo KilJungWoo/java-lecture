cbd56e7ba4265b8faeb6cbc7818d8dddae573a64
스폰지밥 <sado816@naver.com>
2021-12-23 22:01:56 +0900

    비트연산

--- Day03/src/TypeCastingExample.java
+++ Day03/src/TypeCastingExample.java
@@ -2,8 +2,18 @@
 public class TypeCastingExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 10; // 4 bytes
+		short b = (short)a; // 2 bytes
 
+		
+		short c = 100; // 2 bytes
+		int d = c; // 4 bytes
+		
+		
+		
+		int a1 = 128;
+		byte x1 = (byte)a1; // 쓰레기값이 들어감 (-128)
 	}
 
 }
